de6be57ebc5035e5db7ee65ade4e774189ab3c2e
niksON <gromyakov05@mail.ru>
2020-07-20 12:25:51 +0300

    master

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,21 +1,8 @@
 import React from 'react'
 import './style.css'
-// import { useHistory } from 'react-router-dom'
-import { GetRepActionCreater } from '../redux/state'
 
 function Page(props) {
-    debugger
 
-    // const history = useHistory()
-
-    // if ((props.page.lastGetUrl !== history.location.pathname) && (history.location.pathname !== '')) {
-    //     console.log(history.location.pathname)
-    //     const name = history.location.pathname.split('/')[3]
-    //     const owner = history.location.pathname.split('/')[2]
-
-    //     console.log(name + ' ; ' + owner)
-    //     props.dispatch(GetRepActionCreater(name, owner))
-    // }
 
     return (
         <div className='Page'>
@@ -25,10 +12,6 @@ function Page(props) {
                 <img src={props.page.avatar} />
                 <p>{props.page.nickname}</p>
             </div>
-            {/* <p>Languages:</p>
-            <ul>
-                {props.page.languages.map(data => <li>{data.lang}</li>)}
-            </ul> */}
         </div>
     )
 }
